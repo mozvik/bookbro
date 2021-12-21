efa1b1d9c1d14358bcf4bca051831ccefbc1d35f
mozvik <mozvik@yahoo.com>
2021-12-21 11:12:36 +0100

    star ratings, page counts

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -10,6 +10,7 @@ import { ResultsDataService } from 'src/app/service/results-data.service';
 export class DetailsComponent implements OnInit {
   public selectedId: any
   public book: any
+  public stars: string = ''
 
   constructor(
     private route: ActivatedRoute,
@@ -20,8 +21,18 @@ export class DetailsComponent implements OnInit {
     if (!this.resultsDataService.data) {
       this.router.navigate(['/'])
     }
-    this.selectedId = this.route.snapshot.paramMap.get('data.id');
+    this.selectedId = this.route.snapshot.paramMap.get('data.id')
     this.book = this.resultsDataService.getBookById(this.selectedId)
+    if (this.book.volumeInfo.averageRating) this.stars = this.generateStarsHTML(this.book.volumeInfo.averageRating)
+    console.log('this.book :>> ', this.book);
   }
 
+  private generateStarsHTML(rating: number): string {
+    let span = '<span>'
+    for (let index = 0; index < Math.floor(rating); index++) {
+      span += '<i class="fa fa-star"></i>'
+    }
+    if (rating % 1 != 0) span += '<i class="fa fa-star-half"></i>'
+    return span += '</span>'
+  }
 }
